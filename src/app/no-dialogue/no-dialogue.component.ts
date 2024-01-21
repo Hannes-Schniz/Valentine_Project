0570627243035f569718de564ef5ae8786d62a7a
hannes.schniz <hannes.schniz@gmx.de>
2024-01-21 14:08:30 +0100

    no dialugue done

--- src/app/no-dialogue/no-dialogue.component.ts
+++ src/app/no-dialogue/no-dialogue.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./no-dialogue.component.scss']
 })
 export class NoDialogueComponent {
+  very= ""
 
+  verysure() {
+    this.very = this.very + "very "
+  }
 }
