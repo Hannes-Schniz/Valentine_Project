b7429e14bf851bf8f16fc882169561d9704269af
hannes.schniz <hannes.schniz@gmx.de>
2024-01-21 15:12:37 +0100

    added yes button

--- src/app/yes-dialogue/yes-dialogue.component.ts
+++ src/app/yes-dialogue/yes-dialogue.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-yes-dialogue',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class YesDialogueComponent {
 
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  moreText = "I love you more"
+
+  you_more_button() {
+    this.moreText = "No, love you more"
+    const menu = document.querySelector('#animateMore')!;
+    menu.classList.remove('animate');
+    setTimeout(() => {
+      menu.classList.add('animate');
+    }, 1);
+  }
 }
